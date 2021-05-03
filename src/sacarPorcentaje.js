6b3fcbca73feadbcc83bbbf136f8d6f424e1838f
Oscar <romualdoromualdo181@gmail.com>
2021-05-02 19:22:39 -0500

    Export Function sacarPorcentaje

--- src/sacarPorcentaje.js
+++ src/sacarPorcentaje.js
@@ -2,6 +2,4 @@ function sacarPorcentaje(a, b) {
     return a * Number('0.'+b);
 }
 
-console.log(sacarPorcentaje(50, 500));
-
-//module.exports = sacarPorcentaje;
+module.exports = sacarPorcentaje;
